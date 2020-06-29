ac6ffd6271f96886f58038c4332848c3d895cb9c
matheuspr96 <matheuspr96@hotmail.com>
2020-06-28 23:06:57 -0300

    [ADD] Componente login criado, validações formulário criados.

--- FinanApp.API/ClientApp/src/app/app.module.ts
+++ FinanApp.API/ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { UsuarioComponent } from './usuario/usuario.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { UsuarioComponent } from './usuario/usuario.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    UsuarioComponent
+    UsuarioComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -25,7 +28,8 @@ import { UsuarioComponent } from './usuario/usuario.component';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
-      { path: 'usuario', component: UsuarioComponent}
+      { path: 'usuario', component: UsuarioComponent},
+      { path: 'login', component: LoginComponent}
     ])
   ],
   providers: [],
--- FinanApp.API/Startup.cs
+++ FinanApp.API/Startup.cs
@@ -72,6 +72,7 @@ namespace FinanApp.WebAPI
                 if (env.IsDevelopment())
                 {
                     //spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200/");
                 }
             });
         }
