fa9882eebd93ca382c25f91f3d483eef5053db33
matheuspr96 <matheuspr96@hotmail.com>
2020-06-30 21:20:05 -0300

    [UPDATE] Login finalizado com roteamento

--- FinanApp.API/ClientApp/src/app/autorizacao/guarda.rotas.ts
+++ FinanApp.API/ClientApp/src/app/autorizacao/guarda.rotas.ts
@@ -10,7 +10,7 @@ export class GuardaRotas implements CanActivate{
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
         var autenticado =  sessionStorage.getItem("usuario-autenticado");
-        if(autenticado = "1"){
+        if(autenticado == "1"){
             return true;
         }
         this.router.navigate(['login'],{queryParams:{return:state.url}});
--- FinanApp.API/ClientApp/src/app/login/login.component.ts
+++ FinanApp.API/ClientApp/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../model/usuario';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-login',
@@ -11,15 +11,17 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
     public usuario;
     public usuarioAutenticado : boolean;
+    public returnUrl : string;
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private activateRouter: ActivatedRoute) {
         this.usuario = new Usuario();
+        this.returnUrl = this.activateRouter.snapshot.queryParams['return']
     }
-
+    
     login(){
         if(this.usuario.email == 'matheuspr96@hotmail.com' && this.usuario.senha == '@Qwerty1'){
             sessionStorage.setItem("usuario-autenticado","1");
-            this.router.navigate(['/']);
+            this.router.navigate([this.returnUrl]);
         }
     }
 
