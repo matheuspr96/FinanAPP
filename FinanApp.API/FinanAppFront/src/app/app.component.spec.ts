94137a7e11771af0bcd981d6b543b027fc630322
matheuspr96 <matheuspr96@hotmail.com>
2024-02-15 13:20:18 -0300

    add angular 17 and update aspnet core to 6

---
+++ FinanApp.API/FinanAppFront/src/app/app.component.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  let component: AppComponent
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ FinanApp.API/FinanAppFront/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'FinanAppFront';
+}
